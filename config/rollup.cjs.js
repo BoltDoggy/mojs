6ec626f81478bef4f3e4cf5e13ba91b53d0f4d83
王春岩 <Bolt@wangchunyandeMacBook-Pro.local>
2016-10-16 17:38:44 +0800

    update

--- config/rollup.cjs.js
+++ config/rollup.cjs.js
@@ -3,7 +3,11 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
 	entry: 'lib/index.js',
-	plugins: [babel({presets: ['es2015-rollup']}), nodeResolve({jsnext: true})],
+	plugins: [babel({
+		presets: ['es2015-rollup', 'stage-3'],
+		plugins: ['external-helpers-2'],
+	    externalHelpers: true
+	}), nodeResolve({jsnext: true})],
 	format: 'cjs',
 	dest: 'dist/mojs.cjs.js'
 };
--- config/rollup.esnext.js
+++ config/rollup.esnext.js
@@ -1,8 +1,13 @@
+import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
 	entry: 'lib/index.js',
-	plugins: [nodeResolve({jsnext: true})],
+	plugins: [babel({
+		presets: ['stage-3'],
+		plugins: ['external-helpers-2'],
+	    externalHelpers: true
+	}), nodeResolve({jsnext: true})],
 	format: 'es',
 	dest: 'dist/mojs.js'
 };
